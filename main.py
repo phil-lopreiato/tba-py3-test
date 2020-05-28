c408072ed51c5be36b58e0c99d1e8269f25ac529
Phil Lopreiato <plnyyanks@gmail.com>
2020-05-28 17:47:09 -0400

    copy team list page from prod

--- main.py
+++ main.py
@@ -22,6 +22,35 @@ def team_info(team_key: str) -> str:
         return render_template("team.html", team=team)
 
 
+@app.route("/team/list")
+def team_list() -> str:
+    page = 1
+    client = ndb.Client()
+    with client.context():
+        page_labels = ['1-999']
+        cur_page_label = page_labels[0]
+
+        teams_1 = Team.query(Team.team_number >= 0, Team.team_number <= 500).fetch_async()
+        teams_2 = Team.query(Team.team_number >= 501, Team.team_number <= 1000).fetch_async()
+        teams = teams_1.get_result() + teams_2.get_result()
+
+        num_teams = len(teams)
+        middle_value = num_teams // 2
+        if num_teams % 2 != 0:
+            middle_value += 1
+        teams_a, teams_b = teams[:middle_value], teams[middle_value:]
+
+        template_values = {
+            "teams_a": teams_a,
+            "teams_b": teams_b,
+            "num_teams": num_teams,
+            "page_labels": page_labels,
+            "cur_page_label": cur_page_label,
+            "current_page": page
+        }
+        return render_template("team_list.html", **template_values)
+
+
 if __name__ == "__main__":
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
